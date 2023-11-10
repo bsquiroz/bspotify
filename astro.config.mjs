8c7634136dfd676a91e8ddd0a8ad9c7b34d0985b
bsquiroz <bemq1605@gmail.com>
2023-11-09 20:43:25 -0500

    fix bug in the adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
