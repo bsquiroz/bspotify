48cf71e414fea753f088ee4441e96f14c6ae03e8
bsquiroz <bemq1605@gmail.com>
2023-11-09 20:19:43 -0500

    add adaptet

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), react()],
-  output: "server"
+	integrations: [tailwind(), svelte(), react()],
+	output: "server",
+	adapter: vercel(),
 });
